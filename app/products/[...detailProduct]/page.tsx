4848f6373b1a1b11a045e03cacbd8f9e81904d68
JonathanVargas0111 <jonathanfelipe0111@hotmail.com>
2023-08-25 13:16:40 -0500

    Correcion null [...detailProduct] chapuchada

--- app/products/[...detailProduct]/page.tsx
+++ app/products/[...detailProduct]/page.tsx
@@ -1,7 +1,7 @@
 import { Product } from '@/app/types';
 import DetailProduct from '@/app/components/DetailProduct/DetailProduct'
 
-async function getProductById(idProduct: string): Promise<Product> {
+async function getProductById(idProduct: string): Promise<Product | null> {
     try {
         const response = await fetch('https://fakestoreapi.com/products/' + idProduct);
         const data = await response.json();
